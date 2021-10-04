ffd9e1104dc62904d8ffbd0202c66a06ea561858
Dismas <dimich14@gmail.com>
2021-10-04 18:03:27 +0300

    first stable commit

---
+++ src/main/java/com/dismas/PowerShellExecutor/PowerShellExecutorApplication.java
@@ -0,0 +1,33 @@
+package com.dismas.PowerShellExecutor;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+
+
+@SpringBootApplication
+public class PowerShellExecutorApplication implements CommandLineRunner {
+
+	String cmd = "schtasks /query /fo LIST";
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerShellExecutorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws IOException {
+		Process process = Runtime.getRuntime().exec(cmd);
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream(),"windows-1251"));
+		String bufferBool;
+
+		FileWriter writer = new FileWriter("D:\\idea_projects\\PowerShellExecutor\\test.txt");
+		BufferedWriter bw = new BufferedWriter(writer);
+		while ((bufferBool = stdInput.readLine()) != null) {
+			bw.write(bufferBool + " \n");
+		}
+		stdInput.close();
+	}
+
+}
---
+++ src/test/java/com/dismas/PowerShellExecutor/PowerShellExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dismas.PowerShellExecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerShellExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
