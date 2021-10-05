ab689924e67c67643933f59bb10748bc6d1eb84b
Dismas <dimich14@gmail.com>
2021-10-05 12:30:44 +0300

    print task in file

--- src/main/java/com/dismas/PowerShellExecutor/PowerShellExecutorApplication.java
+++ src/main/java/com/dismas/PowerShellExecutor/PowerShellExecutorApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -21,13 +22,20 @@ public class PowerShellExecutorApplication implements CommandLineRunner {
 		Process process = Runtime.getRuntime().exec(cmd);
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream(),"windows-1251"));
 		String bufferBool;
-
+		String allTasksStr = "";
+		String [] strs;
 		FileWriter writer = new FileWriter("D:\\idea_projects\\PowerShellExecutor\\test.txt");
 		BufferedWriter bw = new BufferedWriter(writer);
 		while ((bufferBool = stdInput.readLine()) != null) {
-			bw.write(bufferBool + " \n");
+			allTasksStr = allTasksStr + bufferBool + " \n";
 		}
 		stdInput.close();
+		strs = allTasksStr.split("\\n\\s");
+		for (String str : strs) {
+			bw.write(str);
+			bw.newLine();
+		}
+
 	}
 
 }
